d8af5253a831fd919154ed55014515866d0a5671
JITHESH <jitheshkramesh@hotmail.com>
2019-05-15 15:01:00 +0400

    Home page top side

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'pages/home_page.dart';
 import 'package:flutter/material.dart';
 
+// Color firstColor = Color(0xffdf7599);
+// Color secondColor = Color(0xffdf7599);
+//ThemeData appTheme = ThemeData(primaryColor: Colors.lightBlueAccent);
 void main() => runApp(MaterialApp(
       title: "Flight Tickets Mock Up",
       debugShowCheckedModeBanner: false,
       home: HomePage(),
+      //theme: appTheme,
     ));
--- lib/pages/home_top.dart
+++ lib/pages/home_top.dart
@@ -1,15 +1,23 @@
 import '../pages/custom_shape_clipper.dart';
 import 'package:flutter/material.dart';
 
-Color firstColor = Color(0xFFF47015);
-Color secondColor = Color(0xFFEF4772C);
+// Color firstColor = Color(0xffdf7599);
+// Color secondColor =Color(0xffdf7599);
 
 class HomeTop extends StatefulWidget {
   @override
   _HomeTopState createState() => _HomeTopState();
 }
 
+List<String> locations = ['Boston', 'New York City'];
+const TextStyle dropdownLabelStyle =
+    TextStyle(color: Colors.white, fontSize: 16.0);
+const TextStyle dropdownMenuItemStyle =
+    TextStyle(color: Colors.black, fontSize: 16.0);
+
 class _HomeTopState extends State<HomeTop> {
+  var selectedLocationIndex = 0;
+  var isFlightSelected = true;
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -19,12 +27,189 @@ class _HomeTopState extends State<HomeTop> {
           child: Container(
             height: 400.0,
             decoration: BoxDecoration(
-                gradient: LinearGradient(
-              colors: [firstColor, secondColor],
-            )),
+              gradient: LinearGradient(
+                colors: [Colors.red, Colors.green],
+              ),
+            ),
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 50.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.location_on,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 16.0,
+                      ),
+                      PopupMenuButton(
+                        onSelected: (index) {
+                          setState(() {
+                            selectedLocationIndex = index;
+                          });
+                        },
+                        child: Row(
+                          children: <Widget>[
+                            Text(
+                              locations[selectedLocationIndex],
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 16.0),
+                            ),
+                            Icon(
+                              Icons.arrow_downward,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
+                        itemBuilder: (BuildContext context) =>
+                            <PopupMenuItem<int>>[
+                              PopupMenuItem(
+                                child: Text(
+                                  locations[0],
+                                  style: dropdownMenuItemStyle,
+                                ),
+                                value: 0,
+                              ),
+                              PopupMenuItem(
+                                child: Text(
+                                  locations[1],
+                                  style: dropdownMenuItemStyle,
+                                ),
+                                value: 1,
+                              )
+                            ],
+                      ),
+                      Spacer(),
+                      Icon(
+                        Icons.settings,
+                        color: Colors.black,
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 50.0,
+                ),
+                Text(
+                  "Where would you want to go ?",
+                  style: TextStyle(fontSize: 24.0, color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: TextField(
+                        controller: TextEditingController(
+                          text: locations[1],
+                        ),
+                        style: dropdownMenuItemStyle,
+                        cursorColor: Colors.amberAccent,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            suffix: Material(
+                              elevation: 2.0,
+                              borderRadius: BorderRadius.circular(20.0),
+                              child: Icon(
+                                Icons.search,
+                                color: Colors.black,
+                              ),
+                            )),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    InkWell(
+                      onTap: () {
+                        setState(() {
+                          isFlightSelected = true;
+                        });
+                      },
+                      child: ChoiceChip(
+                          Icons.flight_takeoff, "Flights", isFlightSelected),
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        setState(() {
+                          isFlightSelected = false;
+                        });
+                      },
+                      child:
+                          ChoiceChip(Icons.hotel, "Hotels", !isFlightSelected),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ],
+    );
+  }
+}
+
+class ChoiceChip extends StatefulWidget {
+  final IconData iconData;
+  final String text;
+  final bool isSelected;
+  ChoiceChip(this.iconData, this.text, this.isSelected);
+
+  @override
+  _ChoiceChipState createState() => _ChoiceChipState();
+}
+
+class _ChoiceChipState extends State<ChoiceChip> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+      decoration: widget.isSelected
+          ? BoxDecoration(
+              color: Colors.white.withOpacity(0.15),
+              borderRadius: BorderRadius.all(
+                Radius.circular(20.0),
+              ),
+            )
+          : null,
+      child: Row(
+        children: <Widget>[
+          Icon(
+            widget.iconData,
+            size: 20.0,
+            color: Colors.white,
+          ),
+          SizedBox(
+            width: 8.0,
+          ),
+          Text(
+            widget.text,
+            style: TextStyle(color: Colors.white, fontSize: 14.0),
+          )
+        ],
+      ),
     );
   }
 }
